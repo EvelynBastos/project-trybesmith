c3e44e4dd5f25bc92c7b077a534f1ed5ef323abc
EvelynBastos <lynbastos@icloud.com>
2024-02-20 23:01:49 -0300

    creating test integration products/create and product mock

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,28 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import ProductModel from '../../../src/database/models/product.model';
+import { productCreatedSucessMock, productMock } from '../../mocks/product.mock';
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
-  beforeEach(function () { sinon.restore(); });
+  beforeEach(async function () { sinon.restore(); });
 
+  it('verifica se o produto foi criado com sucesso', async function () {
+    // Arrange
+    const httpResquestBody = productMock;
+    const productCreatedReturn = ProductModel.build(productCreatedSucessMock);
+    sinon.stub(ProductModel, 'create').resolves(productCreatedReturn);
+
+    // Act
+    const httpResponse = await chai.request(app)
+      .post('/products')
+      .send(httpResquestBody);
+    
+    // Assert
+    expect(httpResponse.status).to.equal(201);
+    expect(httpResponse.body).to.be.deep.equal(productCreatedSucessMock);
+  });
 });
---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,12 @@
+export const productMock = {
+    name: "Martelo de Thor",
+    price: "30 peças de ouro",
+    userId: 1
+  }
+
+export const productCreatedSucessMock = {
+    id: 6,
+    name: "Martelo de Thor",
+    price: "30 peças de ouro",
+    userId: 1
+  }
