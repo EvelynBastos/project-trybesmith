17c0c9f3c08b2039c55b29a539f13dbb4ab2d26c
EvelynBastos <lynbastos@icloud.com>
2024-02-20 23:05:31 -0300

    creating test products service

--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,18 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import ProductModel from '../../../src/database/models/product.model';
+import { productCreatedSucessMock } from '../../mocks/product.mock';
+import productService from '../../../src/services/product.service';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
+  
+  it('retorna um produto criado', async function () {
+    const createProductReturn = ProductModel.build(productCreatedSucessMock);
+    sinon.stub(ProductModel, 'create').resolves(createProductReturn);
+    
+    const serviceResponse = await productService.createProduct(productCreatedSucessMock);
 
+    expect(serviceResponse).to.be.deep.equal(productCreatedSucessMock);
+  });
 });
