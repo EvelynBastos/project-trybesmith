c3b9de9a2394ae94c0971e2ab12e08b81f54f0f3
EvelynBastos <lynbastos@icloud.com>
2024-02-20 23:21:55 -0300

    creating test product controller

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import { productCreatedSucessMock, productMock } from '../../mocks/product.mock';
+import productController from '../../../src/controllers/product.controller';
+import productService from '../../../src/services/product.service';
 
 chai.use(sinonChai);
 
@@ -14,5 +17,14 @@ describe('ProductsController', function () {
     res.json = sinon.stub().returns(res);
     sinon.restore();
   });
+  it('retorna o produto criado com status 201', async function () {
+    req.body = productMock;
+    
+    sinon.stub(productService, 'createProduct').resolves(productCreatedSucessMock);
 
+    await productController.createProduct(req, res);
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(productCreatedSucessMock);
+  });
 });
